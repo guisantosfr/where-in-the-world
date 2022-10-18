a281e49d7f4613811723b28ad35123a1012df807
Guilherme Santos <santosgui678@gmail.com>
2022-10-18 11:33:07 -0300

    Make search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="nosubmit">
+    <input class="nosubmit" type="search" placeholder="Search for a country...">
+  </form>
+</template>
+
+<style scoped>
+.nosubmit {
+  border: none;
+  padding: 0;
+}
+
+.nosubmit input {
+  background: var(--dark-blue) url('../assets/icons/search.svg') no-repeat 15px center;
+
+  display: block;
+  width: 85%;
+
+  margin: 1.5rem auto;
+  padding: 1rem 1rem 1rem 3.5rem;
+  border-radius: 8px;
+
+  color: var(--white);
+}
+
+input::placeholder {
+  color: var(--white);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <script setup>
 import Header from '../components/Header.vue';
+import SearchBar from '../components/SearchBar.vue';
 </script>
 
 <template>
   <Header />
+  <SearchBar />
 </template>
+
+<style scoped>
+
+</style>
