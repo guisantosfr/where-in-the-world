4e09d8cf72cbc7e0d5e43d2d670859d669d377f9
Guilherme Santos <santosgui678@gmail.com>
2022-10-18 11:07:06 -0300

    Make header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup>
-</script>
-
 <template>
-  <h1>Hello Vue!</h1>
+  <router-view></router-view>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup>
+import moonIcon from '../assets/icons/moon.svg';
+</script>
+
+<template>
+  <header class="header">
+    <h3 class="header__title">Where in the world?</h3>
+
+    <div class="header__toggle">
+      <img :src="moonIcon" alt="Dark mode moon icon">
+      <p>Dark mode</p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  background: var(--dark-blue);
+
+  margin: auto;
+  padding: 2rem 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header__title {
+  font-weight: 600;
+  font-size: 1.25rem;
+}
+
+.header__toggle {
+  display: flex;
+  align-items: center;
+}
+
+.header__toggle img {
+  margin-right: .75rem;
+}
+</style>
